bba923e5fbdcaaf26aaeab589e819250db55dc10
janar liiv <janar.liiv@hotmail.com>
2020-01-21 17:14:25 +0200

    tasklist

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-const form = document.querySelector('#task-form');
-const taskInput = document.querySelector('#task');
-const taskList = document.querySelector('.collection');
+const form = document.querySelector('#book-form');
+const bookInput = document.querySelector('#title');
+const bookInput2 = document.querySelector('#author');
+const bookList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
-const clearBtn = document.querySelector('.clear-tasks')
+const clearBtn = document.querySelector('.clear-books')
 //console.log(filter);
 
 loadEventListeners();
 
 function loadEventListeners(){
-    form.addEventListener('submit', addTask);
-    taskList.addEventListener('click', removeTask);
+    form.addEventListener('submit', addBook);
+    bookList.addEventListener('click', removeBook);
+
 }
 
-function addTask(event){
-
-    console.log(taskInput.value);
-
-    if(taskInput.value == ''){
-        alert("Add a task!");
+function addBook(event){
+    if(bookInput.value === '' || bookInput2.value === '' ){
+        alert('Add a book title and author!');
     }
-
+    //create an li element
     const li = document.createElement('li');
+    //assign a class name to the html element
     li.className = 'collection-item';
-    li.appendChild(document.createTextNode(taskInput.value));
+    //add text content to the li element
+    li.appendChild(document.createTextNode((bookInput.value) + " - " + bookInput2.value));
+    //create an anchor tag 
     const removeLink = document.createElement('a');
-    removeLink.classList = 'delete-item secondary-content';
-    removeLink.innerHTML = 'x';
-    taskList.appendChild(li);
-    taskList.appendChild(removeLink);
-  
-    storeInLocalStorage(taskInput.value);
+    //ad a class name to the removeLink element
+    removeLink.className = 'delete-item secondary-content';
+    removeLink.innerHTML = 'X';
+    li.appendChild(removeLink);
+
+    //add li element to the ul collection
+    bookList.appendChild(li);
+    
+    //store the book in local storage
+    storeInLocalStorage(bookInput.value + bookInput2.value);
     event.preventDefault();
 }
 
-function storeInLocalStorage(task){
-    let tasks;
-    if(localStorage.getItem('tasks') == null)
-    {
-        tasks = [];
-    }else{
-        tasks = JSON.parse(localStorage.getItem('tasks')); 
+function storeInLocalStorage(title, author){
+    //declare an array to read from local storage
+    let books;
+    if(localStorage.getItem('books') === null){
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem('books'));
     }
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    //add the user's book to the books array
+    books.push(title, author);
+    localStorage.setItem('books', JSON.stringify(books));
 }
 
-function removeTask(event){
+function removeBook(event){
+    //check if the area clicked contains a .delete-item element
+
     if(event.target.classList.contains('delete-item')){
-        if(confirm("Are you sure you want to delete the task?")){
+        if(confirm('Are you sure you want to delete the book?')){
+            //remove the entire li element
             event.target.parentElement.remove();
         }
     }
 }
+
